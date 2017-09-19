437e8da1cafe5d4bbc93e5a50f1264bcf14dc085
guangLess <bha.guang@gmail.com>
2017-09-19 10:23:28 -0400

    local changes

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const twitterApp = express();
 
+twitterApp.use(function(request, response, next) {
+    //response.send('this is middleware');
+    var statusCode = response.statusCode;
+    response.json(statusCode);
+    next();
+})
+
 twitterApp.get('/', function(request,response){
     response.send("Hello!");
 });
-
+twitterApp.get('/news', function(request,response){
+    response.send("news saide Hello!");
+});
 
 twitterApp.listen(3000, function(){
     console.log("server listening")    
