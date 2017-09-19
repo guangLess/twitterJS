e84b2df129f1416744b9dd330f669a1507c5b501
Moyouri Bhattacharjee <moyourib@gmail.com>
2017-09-19 12:26:47 -0400

    get,set,engine, render, configure added

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express');
 const twitterApp = express();
 const nunjucks = require( 'nunjucks');
 
-twitterApp.get('/', function(request, response){
-    response.send('Hello!');
-});
-
+// twitterApp.get('/', function(request, response){
+//     response.send('Hello!');
+//     // next();  <- not right order, will send error request (add next as arg later)
+// });
 
 twitterApp.listen(3000, function(){
     console.log('server listening')
@@ -20,8 +20,30 @@ var locals = {
 	]
 };
 
+var newLocals = {
+	sillyTitle: 'ack',
+	term: [
+		{name: 'hif'},
+		{name: 'dfo'},
+		{name: 'Hee'}
+	]
+};
+
+twitterApp.engine('html', nunjucks.render);
+
+
+ //needs a function .render to render html so look for "html"
+twitterApp.set('view engine', 'html'); //normally, view engine is default undefined in the api docs, so we set it to 
+//html so that it doesnt have to be changed every time in each file?
 nunjucks.configure('views', {noCache: true});
-nunjucks.render('index.html', locals, function(err, res){
-	//console.log(locals);
-	console.log(res);
+// nunjucks.configure('views');
+// nunjucks.render('index.html', locals, function(err, res){
+// 	//console.log(locals);
+// 	console.log(res);
+// });
+twitterApp.get('/', function(request, response){
+	console.log(resquest);
+	console.log(response);
+	response.render('index', {locals, newLocals});
+	 //can only 
 });
