1615edd5eb0df0968e676da9f0bb7e7429a14adb
Moyouri Bhattacharjee <moyourib@gmail.com>
2017-09-19 11:11:43 -0400

    added nunjucks.render and .configure

--- app.js
+++ app.js
@@ -2,11 +2,26 @@ const express = require('express');
 const twitterApp = express();
 const nunjucks = require( 'nunjucks');
 
-twitterApp.get('/', function(request,response){
-    response.send("Hello!");
+twitterApp.get('/', function(request, response){
+    response.send('Hello!');
 });
 
 
 twitterApp.listen(3000, function(){
-    console.log("server listening")    
+    console.log('server listening')
+});
+
+var locals = {
+	title: 'An Example',
+	people: [
+		{name: 'Gandalf'},
+		{name: 'Frodo'},
+		{name: 'Hermione'}
+	]
+};
+
+nunjucks.configure('views', {noCache: true});
+nunjucks.render('index.html', locals, function(err, res){
+	//console.log(locals);
+	console.log(res);
 });
